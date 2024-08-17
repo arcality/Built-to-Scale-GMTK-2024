5fbf5b9dbfca183fce31e988d3795cd97e436eff
Spiider <spiiderdev80@gmail.com>
2024-08-17 16:36:05 -0500

    added dashing mechanic
    and also cleaned up file locations

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,10 +5,12 @@ var gravity := 2000.0
 var speed := 10000.0
 var jump_strength := -600.0
 var special_jump_used := false
+var facing_direction := 1.0
 
-var active_jetpack_state : String
+@export var active_jetpack_state : String
 # can be jumping (i think that will work) or dashing or umbrellaing
 
+# DISREGARD THIS
 # maybe i can signal from the states to here, and determine which action to switch to
 # depending on the current abilities
 # for example, while in the jumping state, I can send a signal to the player class,
@@ -16,25 +18,32 @@ var active_jetpack_state : String
 # whether or not to transition to the airdash or doublejump state, depending on
 # the equipped ability
 
+# MAYBE COME BACK TO THIS
 # maybe I should have all of the state transition code in here, so that the states
 # themselves do not need to worry about that
 # however, state changes may be dependent on which state the player is currently in,
 # so it could be easier to change to a state from the state itself
 
+# ** USING THIS ** active_jetpack_state
 # i could have a variable in the player class that corresponds with the name of
 # a state, and when a button, such as jump, is pressed, the current state switches to
 # that state, accessing a single location whose value is determined by the player class
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	active_jetpack_state = "jumping"
+	#active_jetpack_state = "dashing"
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
+	
+	# stores a facing direction that is never zero/equal to the last direction
+	# input
+	if movement_direction() != 0:
+		facing_direction = movement_direction()
 	
 	move_and_slide()
 	
---
+++ scenes/state-machine/dashing.gd
@@ -0,0 +1,19 @@
+extends State
+
+var player : Player
+
+var dash_speed = 1000
+
+func Enter():
+	player = owner
+	player.velocity.x = dash_speed * player.facing_direction
+	$Timer.start()
+	
+func Update(delta:float):
+	# generate particles or smth
+	pass
+	
+
+
+func _on_timer_timeout():
+	state_transition.emit(self, "falling")
