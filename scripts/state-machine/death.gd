d53fa75e0d69018dce6c2357d2ee7b105849d908
arcality <arcality.dev@gmail.com>
2024-08-20 04:05:34 -0500

    added death animation

--- scripts/state-machine/death.gd
+++ scripts/state-machine/death.gd
@@ -4,13 +4,11 @@ var player : Player
 
 func Enter():
 	player = owner
-	
 
 
 func Update(_delta:float):
 	player.position = player.spawn_position
 	state_transition.emit(self, "idle")
-	
 
 
 func Exit():
